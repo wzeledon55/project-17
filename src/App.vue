0eec8f9c8265599cd76b72b32a75bde0fa10d3ee
William Zeledon <wzeledon55@gmail.com>
2017-10-01 04:06:36 -0700

    logon stuff
    Signed-off-by: William Zeledon <wzeledon55@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,14 @@
     <v-toolbar-title>Project 17</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-side-icon class="hidden-sm-and-up"></v-toolbar-side-icon>
-    <v-toolbar-items class="hidden-xs-only">
+    <v-toolbar-items class="hidden-xs-only" v-show="!visi">
       <v-btn flat router to="/overview">Overview</v-btn>
       <v-btn flat router to="/assignment">Assignment</v-btn>
-      <v-btn flat router to="/settings">Settings</v-btn></v-btn>
-      <v-btn flat router to="/logout">Logout</v-btn></v-btn>
+      <v-btn flat router to="/settings">Settings</v-btn>
+      <v-btn flat router to="/logout" v-on:click.native="visi = true">Logout</v-btn>
+    </v-toolbar-items>
+    <v-toolbar-items class="hidden-xs-only" v-show="visi" >
+      <v-btn flat router to="/login" v-on:click.native="visi=false">Login</v-btn>
     </v-toolbar-items>
   </v-toolbar>
   <router-view></router-view>
@@ -17,7 +20,12 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      visi: true
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Overview from '@/components/Overview'
 import Assignment from '@/components/Assignment'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/assignment',
       name: 'Assignment',
       component: Assignment
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
